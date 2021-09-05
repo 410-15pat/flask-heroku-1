27835ab44e72bdd7c3e8c00cab231cec2e99a577
410-15pat <87839956+410-15pat@users.noreply.github.com>
2021-09-06 03:14:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
 from flask import Flask, jsonify
-from flask import Flask, render_template, Response
-import cv2
-
-camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen03.stream')
+from flask import Flask, render_template
 
 app = Flask(_name_)
-
-def gen_frames():  
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 data = [
         {
@@ -49,15 +35,7 @@ def Home(name):
 
 @app.route('/name')
 def name():
-    return "<font color=Green>ปุญชรัสมิ์</font> <font color=blue>จันทร์กลม</font> <br> <font color=red>เลขที่15 ม.4/10</font> "
-
-@app.route('/video')
-def index():
-    return render_template('index.html')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return "<font color=blue>สัณหพร รัตนพาหิระ</font> <br> <font color=red>เลขที่26 ม.4/10</font> "
 
 if _name_ == "_main_":
-    app.run(debug=True)
+    app.run(debug=False)
